3a09ad84dd94475a27afc8e34d3bd9a9ce1035e3
Charles Kagiri <charleskagiri@hotmail.com>
2016-01-05 22:40:31 +0300

    element list spec

--- test/element-list-spec.js
+++ test/element-list-spec.js
@@ -1,5 +1,6 @@
 var Operand = require('../lib/Operand');
 var AddOperator = require('../lib/AddOperator');
+var Operation = require('../lib/Operation');
 var ElementList = require('../lib/ElementList');
 var assert = require('assert');
 
@@ -17,5 +18,36 @@ describe('ElementList', function () {
         assert.equal(result.lOperand, lOperand);
         assert.equal(result.op, op);
         assert.equal(result.rOperand, rOperand);
+    });
+
+    it('replaceOperation works', function () {
+        var lOperand = new Operand(0);
+        var op = new AddOperator();
+        var rOperand = new Operand(0);
+        var sut = new ElementList([lOperand, op, rOperand]);
+        var operation = new Operation(lOperand, op, rOperand);
+
+        sut.replaceOperation(operation, new Operand(0));
+
+        var result = sut.findOperation();
+        assert.equal(result, null);
+    });
+
+    it('replaceOperation replaces the correct one', function () {
+        var otherOpd1 = new Operand(0);
+        var otherOp = new AddOperator();
+        var otherOpd2 = new Operand(0);
+        var lOperand = new Operand(0);
+        var op = new AddOperator();
+        var rOperand = new Operand(0);
+        var sut = new ElementList([otherOpd1, otherOp, otherOpd2, lOperand, op, rOperand]);
+        var operation = new Operation(lOperand, op, rOperand);
+        
+        sut.replaceOperation(operation, new Operand(0));
+        // to confirm the replacement was correct, findOperation should return the "other" one
+        var result = sut.findOperation();
+        assert.equal(result.lOperand, otherOpd1);
+        assert.equal(result.op, otherOp);
+        assert.equal(result.rOperand, otherOpd2);
     });
 });
